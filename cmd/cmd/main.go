4858d1b3e05c37f4a26f4373394291b448921705
fengxinlei <fengxinlei@360.cn>
2021-07-16 18:11:39 +0800

    feat: 新增websocket处理

--- cmd/cmd/main.go
+++ cmd/cmd/main.go
@@ -5,15 +5,18 @@
 package main
 
 import (
-
 	"github.com/gin-gonic/gin"
 	"net/http"
+
 	"push/internal/app/routers"
+	"push/servers/websocket"
 )
-func main(){
+
+func main() {
 	router := gin.Default()
 	routers.Init(router)
 
-
+	go websocket.StartWebSocket()
 	http.ListenAndServe(":"+"7777", router)
+	//router.Run(":"+"7777")
 }
---
+++ servers/websocket/websocket.go
@@ -0,0 +1,64 @@
+/**
+ * @author fengxinlei
+ * @date 2021/7/16 17:33
+ */
+package websocket
+
+import (
+	"fmt"
+	"github.com/gorilla/websocket"
+	"net/http"
+)
+
+func StartWebSocket() {
+	http.HandleFunc("/push", WsPush)
+	fmt.Println("WebSocket 启动程序成功","ip" , "6666")
+
+	http.ListenAndServe(":"+"8888", nil)
+}
+
+func WsPush(w http.ResponseWriter, req *http.Request) {
+	conn, err := (&websocket.Upgrader{CheckOrigin: func(r *http.Request) bool {
+		return true
+	}}).Upgrade(w, req, nil)
+	if err != nil {
+		http.NotFound(w, req)
+		return
+	}
+	conn.WriteMessage(websocket.TextMessage,[]byte("hello"))
+
+	for {
+		_, data, err := conn.ReadMessage()
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+		err = conn.WriteMessage(websocket.TextMessage, data)
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+	}
+}
+
+/**
+// 升级协议
+	conn, err := (&websocket.Upgrader{CheckOrigin: func(r *http.Request) bool {
+		fmt.Println("升级协议", "ua:", r.Header["User-Agent"], "referer:", r.Header["Referer"])
+
+		return true
+	}}).Upgrade(w, req, nil)
+	if err != nil {
+		http.NotFound(w, req)
+
+		return
+	}
+
+	fmt.Println("webSocket 建立连接:", conn.RemoteAddr().String())
+
+	currentTime := uint64(time.Now().Unix())
+	client := NewClient(conn.RemoteAddr().String(), conn, currentTime)
+
+	go client.read()
+	go client.write()
+*/
